6b5ccb19c98c775f6d2ce95e32324772435e78ab
Griffin <griffin@yardzen.com>
2025-10-29 20:54:41 -0400

    Prevents health endpoint from logging

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -2,4 +2,7 @@ Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::KeyValue.new
   config.lograge.base_controller_class = 'ActionController::API'
+  
+  # Exclude health endpoint from request logging
+  config.lograge.ignore_actions = ['health#index']
 end
