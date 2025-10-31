96fb291d5d4a595041579727d5e5edbba3c4c774
Griffin <griffin@yardzen.com>
2025-10-31 14:02:58 -0400

    Lots of changes

--- app/sidekiq/sidekiq_job.rb
+++
@@ -1,6 +0,0 @@
-class SidekiqJob
-    include Sidekiq::Job
-
-    sidekiq_options retry: 5
-    queue_as :default  
-end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,12 +46,12 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
+  config.require_master_key = false
   # Append comments with runtime information tags to SQL queries in logs.
   config.active_record.query_log_tags_enabled = true
 
   # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
+  # config.active_job.verbose_enqueue_logs = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
